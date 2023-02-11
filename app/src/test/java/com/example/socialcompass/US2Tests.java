a0c9659da655bc1511ecd3d53744fdfa6c12394f
Josh Turman <joshturman@Joshs-MBP.attlocal.net>
2023-02-11 14:34:13 -0800

    added a new test to US2Tests

--- app/src/test/java/com/example/socialcompass/US2Tests.java
+++ app/src/test/java/com/example/socialcompass/US2Tests.java
@@ -1,2 +1,19 @@
-package com.example.socialcompass;public class US2Tests {
+package com.example.socialcompass;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class US2Tests {
+
+    @Test
+    public void blankTest() {
+        assertEquals(1, 1);
+        assertEquals(2, 2);
+    }
 }
