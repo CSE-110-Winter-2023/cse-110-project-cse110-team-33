66bdd57b5b01fa6b736393ba2d708c3a0fb279db
PinaColala <jesse020514@hotmail.com>
2023-02-11 12:25:14 -0800

    LocationService Added

--- app/src/main/java/com/example/socialcompass/MainActivity.java
+++ app/src/main/java/com/example/socialcompass/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,6 +12,7 @@ import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.Manifest;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,22 +22,22 @@ public class MainActivity extends AppCompatActivity {
 
     private OrientationService orientationService;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        orientationService = new OrientationService(this);
-        TextView orientationDisplay = findViewById(R.id.orientationDisplay);
-
-        orientationService.getOrientation().observe(this, orientation -> {
-            orientationDisplay.setText(Float.toString(orientation));
-        });
-
-//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-//        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
-//                SensorManager.SENSOR_DELAY_NORMAL);
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        orientationService = new OrientationService(this);
+//        TextView orientationDisplay = findViewById(R.id.orientationDisplay);
+//
+//        orientationService.getOrientation().observe(this, orientation -> {
+//            orientationDisplay.setText(Float.toString(orientation));
+//        });
+//
+////        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+////        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+////                SensorManager.SENSOR_DELAY_NORMAL);
+//    }
 
     @Override
     protected void onPause() {
@@ -55,4 +58,25 @@ public class MainActivity extends AppCompatActivity {
 //    public void onAccuracyChanged(Sensor sensor, int i) {
 //
 //    }
+
+    private LocationService locationService;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+
+        locationService = LocationService.singleton(this);
+
+        TextView textview = (TextView) findViewById(R.id.locationDisplay);
+
+        locationService.getLocation().observe(this, loc ->{
+            textview.setText(Double.toString(loc.first) + " , " + Double.toString(loc.second));
+        });
+    }
 }
