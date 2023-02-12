3dba551891400ba49765d2768b5585c6e92835f8
Bryce Pollack <bnpollack@ucsd.edu>
2023-02-11 21:58:36 -0800

    Added US2

---
+++ app/src/main/java/com/example/socialcompass/AngleCalculation.java
@@ -0,0 +1,29 @@
+package com.example.socialcompass;
+
+import static java.lang.Math.*;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class AngleCalculation {
+
+
+    private static final double RADIANS_TO_DEGREES = 180 / PI;
+    private static final int EARTH_RADIUS = 6371; // Approx Earth radius in KM
+    public static double calculateBearing(double lat1, double long1, double lat2, double long2) {
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+        double deltaLongitude = Math.toRadians(long2 - long1);
+
+        double y = Math.sin(deltaLongitude) * Math.cos(lat2);
+        double x = Math.cos(lat1) * Math.sin(lat2) -
+                Math.sin(lat1) * Math.cos(lat2) * Math.cos(deltaLongitude);
+        double bearing = Math.atan2(y, x);
+
+        bearing = Math.toDegrees(bearing);
+        if (bearing < 0) {
+            bearing += 360;
+        }
+
+        return bearing;
+    }
+}
--- app/src/main/java/com/example/socialcompass/MainActivity.java
+++ app/src/main/java/com/example/socialcompass/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 
 import android.content.Context;
@@ -12,6 +13,7 @@ import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.Manifest;
@@ -21,8 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
     private OrientationService orientationService;
     private LocationService locationService;
+    private AngleCalculation calculator;
 
     private ImageView compassDisplay;
+    private ImageView circle;
+    private ConstraintLayout compassConstraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         compassDisplay = findViewById(R.id.compassDisplay);
+        compassConstraintLayout = findViewById(R.id.compassConstraintLayout);
+
+        //Parent's house
+        circle = findViewById(R.id.circle);
+        Pair<Double, Double> parent_house = new Pair<Double, Double>(40.7128, -74.0060);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
         && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
@@ -44,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
         locationService.getLocation().observe(this, loc ->{
             textview.setText(Double.toString(loc.first) + " , " + Double.toString(loc.second));
             Log.d("LOCATION", Double.toString(loc.first));
+            double relative_angle = calculator.calculateBearing(loc.first, loc.second, parent_house.first, parent_house.second);
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) circle.getLayoutParams();
+            layoutParams.circleAngle = (float) relative_angle;
+            circle.setLayoutParams(layoutParams);
         });
 
         //Initialize, pull, and display orientation
@@ -53,8 +67,10 @@ public class MainActivity extends AppCompatActivity {
         orientationService.getOrientation().observe(this, orientation -> {
             //orientationDisplay.setText(Float.toString(orientation));
             orientationDisplay.setText(String.format("%.2f", orientation));
-            compassDisplay.setRotation((float) (-orientation*180/3.14159));
+            compassConstraintLayout.setRotation((float) (-orientation*180/3.14159));
         });
+
+
     }
 
     @Override
