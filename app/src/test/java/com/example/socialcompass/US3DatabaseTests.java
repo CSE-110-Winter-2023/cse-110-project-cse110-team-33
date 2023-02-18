6ee93f5150dd00aa139c97f6f3140544076d28c1
Josh Turman <joshturman@Joshs-MBP.attlocal.net>
2023-02-18 14:41:12 -0800

    US3 - set up robolectric test framework

--- app/src/test/java/com/example/socialcompass/US3DatabaseTests.java
+++ app/src/test/java/com/example/socialcompass/US3DatabaseTests.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+//import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
