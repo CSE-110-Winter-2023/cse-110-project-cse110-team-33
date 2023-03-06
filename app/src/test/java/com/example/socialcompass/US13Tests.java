e3237a216e82da2db5fa0738e320e76f8476f88e
Josh Turman <joshturman@Joshs-MacBook-Pro.local>
2023-03-06 13:49:53 -0800

    US13-1: added a unit test, but it failed, will fix algoithm soon

--- app/src/test/java/com/example/socialcompass/US13Tests.java
+++ app/src/test/java/com/example/socialcompass/US13Tests.java
@@ -8,14 +8,17 @@ import org.junit.Test;
 
 public class US13Tests {
 
+    // accepted error range
+    private final double DELTA = 1e-9;
+
     @Test
     public void testCalcualteDistance() {
         DistanceCalculation calculator = new DistanceCalculation();
-        double lat1;
-        double long1;
-        double lat2;
-        double long2;
+        double lat1 = 0;
+        double long1 = 0;
+        double lat2 = 90;
+        double long2 = 90;
 
-        assertEquals(1, calculator.CalculateDistance(lat1, long1, lat2, long2));
+        assertEquals(10007.543398, calculator.CalculateDistance(lat1, long1, lat2, long2), DELTA);
     }
 }
