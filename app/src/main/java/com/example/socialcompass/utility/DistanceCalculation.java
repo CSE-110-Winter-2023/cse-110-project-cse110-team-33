ef40b2bbd76df27482d0855476c663d5bdc121e6
Josh Turman <joshturman@Joshs-MacBook-Pro.local>
2023-03-06 13:30:07 -0800

    US13-1: implemented the DistanceCalculation algorithm

--- app/src/main/java/com/example/socialcompass/utility/DistanceCalculation.java
+++ app/src/main/java/com/example/socialcompass/utility/DistanceCalculation.java
@@ -1,4 +1,25 @@
 package com.example.socialcompass.utility;
 
 public class DistanceCalculation {
+
+    private static final int EARTH_RADIUS = 6371; // Approx Earth radius in KM
+
+    /**
+     * returns the distance (in kilometers) between two locations, given the longitude and latitude
+     * of the two locations
+     *
+     * @param userLat Latitude of the user
+     * @param userLong Longitude of the user
+     * @param friendLat Latitude of the friend
+     * @param friendLong Longitude of the friend
+     * @return the distance, in kilometers, between the two given locations
+     */
+    public double CalculateDistance(double userLat, double userLong, double friendLat, double friendLong) {
+        double sinArgument = Math.sin(userLat) * Math.sin(friendLat);
+        double cosArgument = Math.cos(userLat) * Math.cos(friendLat) * Math.cos(friendLong - userLong);
+
+        double distance = Math.acos(sinArgument + cosArgument) * EARTH_RADIUS;
+
+        return distance;
+    }
 }
